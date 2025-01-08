ab64fc3d0d96272e229bf22e8b0633c80df97799
OTWhite04 <owen.t.white2005@gmail.com>
2025-01-08 08:27:33 -0400

    Initial commit

---
+++ Assets/Scripts/ActionsManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviorScript : MonoBehaviour
+{
+    public static class Actions
+    {
+        public static Action Spotlight;
+
+        public static Action WallAnimation;
+
+        public static Action WallVanish;
+
+    }
+
+
+}
---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeWallScript : MonoBehaviour
+{
+    public GameObject Light;
+
+    void EnableLight()
+    {
+        //Actions.Spotlight?.Invoke();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Actions.Spotlight += EnableLight;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
